776c0db81979cba168ebb4a3e54e1ace4e0cb517
mateus-gomes <mateus.cruz@bandtec.com.br>
2021-12-29 15:09:01 -0300

    Setup application

---
+++ olympus/src/main/java/com/monketabletop/olympus/OlympusApplication.java
@@ -0,0 +1,13 @@
+package com.monketabletop.olympus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlympusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlympusApplication.class, args);
+	}
+
+}
---
+++ olympus/src/test/java/com/monketabletop/olympus/OlympusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monketabletop.olympus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
